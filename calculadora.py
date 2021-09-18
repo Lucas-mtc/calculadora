682ac4e4ce9bf959808fe76104c247e6d670d3ea
lucas <lucas@DESKTOP-5AUU42H>
2021-09-18 00:08:45 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+
+n1 = int(input("Insira um número: "))
+n2 = int(input("Insira outro número: "))
+
+soma = n1 + n2
+subtracao = n1 - n2
+divisao = n1 / n2
+multiplicacao = n1 * n2
+modulo = n1 % n2
+divisaoExata = n1 // n2
+
+print("A soma dos números é:", soma)
+print("A subtração dos números é:", subtracao)
+print("A divisão dos números é:", divisao)
+print("A multiplicação dos números é:", multiplicacao)
+print("O módulo dos números é:", modulo)
+print("A divisão exata dos números é:", divisaoExata)
